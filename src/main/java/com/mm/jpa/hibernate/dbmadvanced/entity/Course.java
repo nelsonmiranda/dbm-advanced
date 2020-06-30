2641b0ea47bfaabe92ea82ae3df4158aee7494a2
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-29 22:52:24 -0600

    Implementing ManyToMany Relationship Between Course and Student

--- src/main/java/com/mm/jpa/hibernate/dbmadvanced/entity/Course.java
+++ src/main/java/com/mm/jpa/hibernate/dbmadvanced/entity/Course.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -29,6 +30,9 @@ public class Course {
 
 	@OneToMany(mappedBy = "course")
 	private List<Review> reviews = new ArrayList<>();	
+	
+	@ManyToMany(mappedBy = "courses")
+	private List<Student> students = new ArrayList<>();
 	
 	@UpdateTimestamp
 	private LocalDateTime lastUpdatedDate;
@@ -70,6 +74,18 @@ public class Course {
 	
 	public void removeReview(Review review) {
 		this.reviews.remove(review);
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void addStudent(Student student) {
+		this.students.add(student);
+	}
+	
+	public void removeStudent(Student student) {
+		this.students.remove(student);
 	}
 
 	@Override
--- src/main/java/com/mm/jpa/hibernate/dbmadvanced/entity/Student.java
+++ src/main/java/com/mm/jpa/hibernate/dbmadvanced/entity/Student.java
@@ -1,9 +1,15 @@
 package com.mm.jpa.hibernate.dbmadvanced.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -18,6 +24,12 @@ public class Student {
 	@OneToOne(fetch = FetchType.LAZY)
 	private Passport passport;
 
+	@ManyToMany
+	@JoinTable(name = "STUDENT_COURSE",
+	joinColumns = @JoinColumn(name="STUDENT_ID"),
+	inverseJoinColumns = @JoinColumn(name="COURSE_ID"))
+	private List<Course> courses = new ArrayList<>();
+	
 	public Student() {
 
 	}
@@ -48,6 +60,18 @@ public class Student {
 
 	public void setPassport(Passport passport) {
 		this.passport = passport;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void addCourse(Course course) {
+		this.courses.add(course);
+	}
+	
+	public void removeCourse(Course course) {
+		this.courses.remove(course);
 	}
 
 	@Override
