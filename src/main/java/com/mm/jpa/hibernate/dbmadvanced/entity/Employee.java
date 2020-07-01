28cb1a93c862f8d16042b441c1b63e512af9f4f4
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-30 18:50:08 -0600

    Inheritance strategy - TABLE PER CLASS

--- src/main/java/com/mm/jpa/hibernate/dbmadvanced/entity/Employee.java
+++ src/main/java/com/mm/jpa/hibernate/dbmadvanced/entity/Employee.java
@@ -9,8 +9,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE )
-@DiscriminatorColumn(name = "EmployeeType")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS )
 public abstract class Employee {
 
 	@Id
