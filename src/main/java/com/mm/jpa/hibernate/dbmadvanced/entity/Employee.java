23bf3698619a577d0d208857a122951f79f6674d
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-30 18:07:12 -0600

    Inheritance strategy - SINGLE_TABLE

--- src/main/java/com/mm/jpa/hibernate/dbmadvanced/entity/Employee.java
+++ src/main/java/com/mm/jpa/hibernate/dbmadvanced/entity/Employee.java
@@ -1,11 +1,16 @@
 package com.mm.jpa.hibernate.dbmadvanced.entity;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE )
+@DiscriminatorColumn(name = "EmployeeType")
 public abstract class Employee {
 
 	@Id
