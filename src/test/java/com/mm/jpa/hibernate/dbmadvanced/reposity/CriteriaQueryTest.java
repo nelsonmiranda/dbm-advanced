55d660783f20c43ff5a2a2d556703e343f33ac04
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-07-06 15:19:51 -0600

    Criteria Query - Queries with Like, isEmpty and Joins.

--- src/test/java/com/mm/jpa/hibernate/dbmadvanced/reposity/CriteriaQueryTest.java
+++ src/test/java/com/mm/jpa/hibernate/dbmadvanced/reposity/CriteriaQueryTest.java
@@ -9,6 +9,8 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.junit.jupiter.api.Test;
@@ -54,5 +56,87 @@ class CriteriaQueryTest {
 		List<Course> resultList = query.getResultList();
 		logger.info("Courses ->{}", resultList);
 	}
+	
+	@Test
+	public void jpql_criteria_like() {
+		// 1. Use Criteria Builder to create a Criteria Query returning the expected
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+		
+		// 2. Define roots for tables which are involved in the query
+		Root<Course> courseRoot = cq.from(Course.class);
+		
+		// 3. Define Predicates etc using Criteria Builder
+		Predicate like = cb.like(courseRoot.get("name"), "%100%");
+		
+		// 4. Add Predicates etc to the Criteria Query
+		cq.where(like);
+		
+		// 5. Build the TypedQuery using the entity manager and criteria query
+		TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+		
+		List<Course> resultList = query.getResultList();
+		logger.info("Courses ->{}", resultList);
+	}
 
+	@Test
+	public void jpql_criteria_empty() {
+		// 1. Use Criteria Builder to create a Criteria Query returning the expected
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+		
+		// 2. Define roots for tables which are involved in the query
+		Root<Course> courseRoot = cq.from(Course.class);
+		
+		// 3. Define Predicates etc using Criteria Builder
+		Predicate isEmpty = cb.isEmpty(courseRoot.get("students"));
+		
+		// 4. Add Predicates etc to the Criteria Query
+		cq.where(isEmpty);
+		
+		// 5. Build the TypedQuery using the entity manager and criteria query
+		TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+		
+		List<Course> resultList = query.getResultList();
+		logger.info("Courses ->{}", resultList);
+//		[Course [id=10002, name=Spring in 10 Steps]]
+	}
+	
+	@Test
+	public void jpql_criteria_join() {
+		// 1. Use Criteria Builder to create a Criteria Query returning the expected
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+		
+		// 2. Define roots for tables which are involved in the query
+		Root<Course> courseRoot = cq.from(Course.class);
+		courseRoot.join("students");
+				
+		// 5. Build the TypedQuery using the entity manager and criteria query
+		TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+		
+		List<Course> resultList = query.getResultList();
+		logger.info("Courses ->{}", resultList);
+//		[Course [id=10001, name=JPA in 10 Steps], Course [id=10001, name=JPA in 10 Steps], Course [id=10001, name=JPA in 10 Steps], 
+//		Course [id=10003, name=Spring Boot in 100 Steps]]
+	}
+	
+	@Test
+	public void jpql_criteria_join_left() {
+		// 1. Use Criteria Builder to create a Criteria Query returning the expected
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+		
+		// 2. Define roots for tables which are involved in the query
+		Root<Course> courseRoot = cq.from(Course.class);
+		courseRoot.join("students", JoinType.LEFT);
+				
+		// 5. Build the TypedQuery using the entity manager and criteria query
+		TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+		
+		List<Course> resultList = query.getResultList();
+		logger.info("Courses ->{}", resultList);
+//		[Course [id=10001, name=JPA in 10 Steps], Course [id=10001, name=JPA in 10 Steps], Course [id=10001, name=JPA in 10 Steps], 
+//		Course [id=10002, name=Spring in 10 Steps], Course [id=10003, name=Spring Boot in 100 Steps]]
+	}
 }
